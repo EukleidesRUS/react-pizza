fb58b763d876ce56855b3eddea54a0b7d16f06ca
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2023-02-07 22:43:02 +0300

    add: interactive selector sort-name

--- src/components/Sort/Sort.jsx
+++ src/components/Sort/Sort.jsx
@@ -2,21 +2,38 @@ import React, { useState } from "react";
 import ArrowSort from "../Icons/arrow-sort/ArrowSort";
 
 const Sort = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [sortName, setSortName] = useState("популярности");
   const [openPopup, setOpenPopup] = useState(false);
+  const list = ["популярности", "цене", "алфавиту"];
+
+  const chooseFilterItem = (name, index) => {
+    setActiveIndex(index);
+    setSortName(name);
+    setOpenPopup(!openPopup);
+  };
 
   return (
     <div className="sort">
       <div className="sort__label">
         <b>Сортировка по:</b>
-        <ArrowSort isOpen={!openPopup}/>
-        <span onClick={() => setOpenPopup(!openPopup)}>популярности</span>
+        <ArrowSort isOpen={openPopup} />
+        <span onClick={() => setOpenPopup(!openPopup)}>{sortName}</span>
       </div>
       {openPopup && (
         <div className="sort__popup">
           <ul>
-            <li className="active">популярности</li>
-            <li>цене</li>
-            <li>алфавиту</li>
+            {list.map((name, index) => {
+              return (
+                <li
+                  onClick={() => chooseFilterItem(name, index)}
+                  className={activeIndex === index ? "active" : ""}
+                  key={name}
+                >
+                  {name}
+                </li>
+              );
+            })}
           </ul>
         </div>
       )}
